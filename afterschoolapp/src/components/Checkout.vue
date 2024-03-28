124c2016443b14cb2c6e6616028fbb242f136dc6
aqeelaman <AA5226@live.mdx.ac.uk>
2024-03-29 00:06:51 +0400

    updated checkout

--- afterschoolapp/src/components/Checkout.vue
+++ afterschoolapp/src/components/Checkout.vue
@@ -2,15 +2,15 @@
   <div>
     <h1>Cart</h1>
     <div id="cartContainer">
-      <div id="cartCardContainer" v-for="id in cart" :key="id">
-        <div id="productCard" v-for="product in filteredProducts" :key="String(product.id)">
+      <div id="cartCardContainer"><!-- v-for="id in cart" :key="id" -->
+        <div id="productCard" v-for="product in filteredProducts" :key="product.id">
           <figure>
             <img :src="product.image" alt="Product Image">
           </figure>
           <h2>{{ product.title }}</h2>
           <p>Location: {{ product.location }}</p>
           <p>Price: AED{{ product.price }}</p>
-          <button @click="removeFromCart(product)">
+          <button @click="removeItemFromCart(product)">
             Remove Item
           </button>
           <br>
@@ -18,28 +18,28 @@
       </div>
     </div>
 
-    <!-- <div id="checkoutContainer">
-          <h1>Checkout</h1>
+    <div id="checkoutContainer">
+      <h1>Checkout</h1>
 
-          <p>
-            <strong>Name:</strong>
-            <input v-model.trim="order.name" />
-          </p>
-          <p>
-            <strong>Phone:</strong>
-            <input type="number" v-model="order.phone" />
-          </p>
-          <br><br><br>
+      <p>
+        <strong>Name:</strong>
+        <input v-model.trim="order.name" />
+      </p>
+      <p>
+        <strong>Phone:</strong>
+        <input type="number" v-model="order.phone" />
+      </p>
+      <br><br><br>
 
-          <h1>Order Information</h1>
-          <p>Name: {{ order.name }}</p>
-          <p>Phone: {{ order.phone }}</p>
+      <h1>Order Information</h1>
+      <p>Name: {{ order.name }}</p>
+      <p>Phone: {{ order.phone }}</p>
 
-          <button v-if="validateInput" v-on:click="submitForm">
-            Place Order</button>
-          <button v-else disabled='disabled'>
-            Place Order</button>
-        </div> -->
+      <button v-if="validateInput" @click="submitForm()">
+        Place Order</button>
+      <button v-else disabled='disabled'>
+        Place Order</button>
+    </div>
 
 
     <!-- <h2>Checkout</h2>
@@ -54,7 +54,7 @@
             <strong>Address</strong>
             <input v-model="address"/>
             <button @click="submitForm">Submit</button>
-        </div> -->
+        </div>-->
   </div>
 </template>
 <script>
@@ -63,25 +63,51 @@ export default {
   props: ['cart', 'products'],
   data() {
     return {
-      name: "",
-      address: ""
+      order: {
+        name: "",
+        phone: "",
+        cart: this.cart
+      }
     };
   },
   methods: {
-    removeFromCart(product) {
+    removeItemFromCart(product) {
       this.$emit('removeFromCart', product);
     },
     submitForm() {
-      console.log("Form submitted with ", this.name, this.address);
-      this.name = "";
-      this.address = "";
+      console.log("clicked");
+      console.log(this.order);
+      this.$emit('submitForm',this.order);
     }
   },
   computed: {
     filteredProducts() {
-      return this.products.filter(product => {
-        return this.cart.includes(product.id);
-      });
+      let filteredProducts = [];
+      for (let item in this.cart) {
+        for (let product in this.products) {
+          //console.log(this.products[product].id +" - "+ this.cart[item]);
+          if (this.products[product].id == this.cart[item]) {
+            filteredProducts.push(this.products[product]);
+          }
+        }
+      }
+      return filteredProducts;
+    },
+    validateInput() {
+
+      var regName = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/g;
+      var regNumber = /^[0-9]*$/g;
+
+      //console.log("name:" + regName.test(this.order.name));
+      //console.log("number:" + regNumber.test(this.order.phone) + " number:" + this.order.phone)
+
+      if (this.order.name != "" && this.order.phone != "") {
+        //console.log("check1");
+        if (regName.test(this.order.name) && regNumber.test(this.order.phone)) {
+          return true;
+        }
+      }
+      return false;
     }
   }
 }
